3e8c7f8aa993ed0a992d2c04e850d4f18cbe7596
Mojens <moha135h@stud.kea.dk>
2022-10-04 13:44:32 +0200

    login virker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let URL_LOGIN = "http://localhost:8080/api/employees/login";
 
 let button = document.getElementById("loginbtn");
-function login() {
+async function login() {
     const loginInfo = {
         userName: document.getElementById("employeeuser").value,
         password: document.getElementById("userpwd").value
@@ -11,18 +11,24 @@ function login() {
     opts.headers = { "Content-type": "application/json" }
     opts.body = JSON.stringify(loginInfo)
 
-    fetch(URL_LOGIN, opts).then(response => {
-        console.log(response.status)
+    await fetch(URL_LOGIN, opts).then(response => {
         if (response.status == 404) {
+            if(sessionStorage.getItem("user")!== null){
+                sessionStorage.removeItem("user");
+            }
             window.location.href = "loginSucces.html";
             throw new Error("User not found");
         } else if (response.status == 202) {
             sessionStorage.setItem("user", "true");
             window.location.href = "loginSucces.html";
         } else if (response.status == 400) {
+            if(sessionStorage.getItem("user")!== null){
+                sessionStorage.removeItem("user");
+            }
             window.location.href = "loginSucces.html";
             throw new Error("Incorrect password");
         }
+        
     })
     /*
     console.log(document.getElementById("employeeuser").value)
